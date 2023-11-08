8d1e02cc50e0727c9fc39ee8b24ee7a58a9f6041
ToanDL1 <toanlevan0123@gmail.com>
2023-11-08 16:44:51 +0700

    intizale code

---
+++ CatchBall2.0/Assets/Scrips/Rectangle.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Rectangle : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move(){
+         
+    }
+}
